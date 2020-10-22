8c56b23fa2c1f24598c92a192231415d0d1d2f62
muitanprasert <ttaa2017@mymail.pomona.edu>
2020-10-22 18:18:29 +0700

    Executables

--- password/src/password/PasswordStrength.java
+++ password/src/password/PasswordStrength.java
@@ -1,70 +1,78 @@
 package password;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 /**
  * 
  * @author Mui, Alex
  *
- * Common passwords from from https://github.com/danielmiessler/SecLists/blob/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt
- * Common dictionary words from https://www.mit.edu/~ecprice/wordlist.10000
+ *         Common passwords from from
+ *         https://github.com/danielmiessler/SecLists/blob/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt
+ *         Common dictionary words from
+ *         https://www.mit.edu/~ecprice/wordlist.10000
  * 
  */
 public class PasswordStrength {
-	
+
 	/**
 	 * Constructor and password strength checker
+	 * 
 	 * @throws IOException
 	 */
 	public PasswordStrength(String pw) throws IOException {
-		
+		if (check_strength(pw))
+			System.out.println("strong");
+		else
+			System.out.println("weak");
+	}
+
+	private boolean check_strength(String pw) throws IOException {
+
 		// at least 8 characters
-		
+		if (pw.length() < 8)
+			return false;
+
 		// all printable characters accepted
 		// perhaps require a number & a symbol to try to enforce this?
-		
-		// not repetitive characters - at least 5 unique characters?
-		
-		// check against 3.5k dictionary words longer than 8 characters
-		
-		// check against 400k common passwords longer than 8 characters
-		
 
-		/*FileWriter myWriter = new FileWriter("./dictionary.txt");
-		for(String word:sb) {
-			if(word.length()>=8)
-				myWriter.write(word+"\n");
-		}
-		myWriter.close();*/
+		// not repetitive characters - at least 5 unique characters?
+
+		// check against 3.5k dictionary words longer than 8 characters
+		if (!check_wordlist(pw, "dictionary.txt"))
+			return false;
+
+		// check against 400k common passwords longer than 8 characters
+		return check_wordlist(pw, "compromised.txt");
 	}
 
 	/**
 	 * Check if pw is in the list the specified file
+	 * 
 	 * @param pw
 	 * @param filename
 	 * @return
 	 * @throws IOException
 	 */
-	private boolean check_wordlist(String pw, String filename) throws IOException{
-		BufferedReader br = new BufferedReader(new FileReader(new File(filename)));
+	private boolean check_wordlist(String pw, String filename) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream(filename)));
 		ArrayList<String> sb = new ArrayList<String>();
 		String line;
-		while((line = br.readLine()) != null)
+		while ((line = br.readLine()) != null)
 			sb.add(line);
 		br.close();
-		
-		if(sb.contains(pw))
+
+		if (sb.contains(pw))
 			return false;
 		return true;
 	}
-	
+
 	/**
 	 * runs the password strength checker
-	 * @throws IOException 
+	 * 
+	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
 		if (args.length < 1)
